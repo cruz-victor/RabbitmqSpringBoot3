86090cba3e036c89c0ce916a0ae7169f4415b33a
Vic <cruz.gomez.victor@gmail.com>
2023-07-07 00:27:24 -0400

    Proyect publish v1.0.0 added

---
+++ publishSubcribeSacavixV1.0.0/src/main/java/com/sacavix/mq/dummy/DummyService.java
@@ -0,0 +1,25 @@
+package com.sacavix.mq.dummy;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.sacavix.mq.publisher.Publisher;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
+public class DummyService {
+
+	@Autowired
+	private Publisher publisher;
+	
+	public void sendToRabbit(String message) {	
+		Data d = new Data(12, message);
+		log.info("Message '{}' will be send ... ", d);
+		//log.info("Message '{}' will be send...",message);
+		this.publisher.send(d);
+		//this.publisher.send(message);
+	}
+	
+}
